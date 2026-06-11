f25af2b8f86a68eeed8440740e0a3d2fb52eb3d8
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-06-11 15:22:38 +0300

    rebuild main

--- NTA_lab2/main.cpp
+++ NTA_lab2/main.cpp
@@ -1,17 +1,21 @@
 #include "header.hpp"
 
 int main(int argc, char* argv[]) {
-	if (argc != 4) {
-		cout << "Usage: " << argv[0] << " <a> <b> <p>" << endl;
-		cout << "Solves: a^x = b (mod p)" << endl;
-		return 1;
+	uint64_t a, b, p;
+
+	if (argc == 4) {
+		a = stoull(argv[1]);
+		b = stoull(argv[2]);
+		p = stoull(argv[3]);
+	}
+	else {
+		cout << "Solving: a^x = b (mod p)" << endl;
+		cout << "Enter a: "; cin >> a;
+		cout << "Enter b: "; cin >> b;
+		cout << "Enter p: "; cin >> p;
 	}
 
-	uint64_t a = stoull(argv[1]);
-	uint64_t b = stoull(argv[2]);
-	uint64_t p = stoull(argv[3]);
-
-	cout << "SPH:        " << sph(a, b, p) << endl;
+	cout << "\nSPH:         " << sph(a, b, p) << endl;
 	cout << "Brute force: " << brute_force(a, b, p) << endl;
 
 	return 0;
