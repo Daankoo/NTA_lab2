455d96eb9427a5f21754992ad14aed2b494129e8
Danko <bogdbutk-ipt28@lll.kpi.ua>
2026-06-11 19:30:02 +0300

    правки

--- NTA_lab2/main.cpp
+++ NTA_lab2/main.cpp
@@ -1,3 +1,20 @@
+/*
+ Оновити після змін :
+ docker build -t daankooo/nta_lab2 .
+
+ Запушити:                    
+ docker push daankooo/nta_lab2
+ 
+ Запуск halper:               
+ docker run -it oyakymchuk/nta_cp2_helper
+ 
+ Зразу значення:              
+ docker run daankooo/nta_lab2 <a> <b> <p>
+ 
+ Поступово:                   
+ docker run - it daankooo/nta_lab2
+*/ 
+
 #include "header.hpp"
 
 int main(int argc, char* argv[]) {
@@ -15,8 +32,23 @@ int main(int argc, char* argv[]) {
 		cout << "Enter p: "; cin >> p;
 	}
 
-	cout << "\nSPH:         " << sph(a, b, p) << endl;
-	cout << "Brute force: " << brute_force(a, b, p) << endl;
+	cout << endl;
+
+	auto t1 = high_resolution_clock::now();
+	uint64_t x_sph = sph(a, b, p);
+	auto t2 = high_resolution_clock::now();
+	long long us_sph = duration_cast<milliseconds>(t2 - t1).count();
+	cout << "SPH:         " << x_sph << " | Time: " << us_sph << " us" << endl;
+
+	auto t3 = high_resolution_clock::now();
+	int64_t x_bf = brute_force(a, b, p);
+	auto t4 = high_resolution_clock::now();
+	long long us_bf = duration_cast<milliseconds>(t4 - t3).count();
+
+	if (x_bf == -1)
+		cout << "Brute force: timeout | Time: " << us_bf << " us" << endl;
+	else
+		cout << "Brute force: " << x_bf << " | Time: " << us_bf << " us" << endl;
 
 	return 0;
 }
